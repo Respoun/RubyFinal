d19ca0ae5cf6812517e10038b765c318dfc25341
MaxDububuche <mdubuche27@gmail.com>
2021-01-22 13:30:41 +0100

    CRUD non teste

--- rubyFinal/app/controllers/beer_controller.rb
+++ rubyFinal/app/controllers/beer_controller.rb
@@ -2,15 +2,21 @@ class BeerController < ApplicationController
     before_action :set_beer, only: [:show, :edit, :update, :destroy]
 
     def index
-        @beer = Beer.all
+        @beers = Beer.all
     end
     
     def new
+        @brewery = Brewery.find(params[:brewery_id])
+        @style = Style.find(params[:style_id])
         @beer = Beer.new
     end
     
     def create
         @beer = Beer.new(beer_params)
+        @brewery = Brewery.find(params[:brewery_id])
+        @style = Style.find(params[:style_id])
+        @beer.brewery = @brewery
+        @beer.style = @style
         @beer.save
         redirect_to beer_path(@beer)
     end
--- rubyFinal/config/routes.rb
+++ rubyFinal/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :beer do 
-    resources :brewery, only[:new, :create]
-    resources :style, only[:new, :create]
+    resources :brewery, only: [:new, :create]
+    resources :style, only: [:new, :create]
   end
 end
