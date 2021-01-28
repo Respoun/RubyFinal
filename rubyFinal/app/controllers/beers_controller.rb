6ceb10c097135c0209f85cab457c4f75fcc4231e
MaxDububuche <mdubuche27@gmail.com>
2021-01-28 15:43:47 +0100

    bug

--- rubyFinal/app/controllers/beers_controller.rb
+++ rubyFinal/app/controllers/beers_controller.rb
@@ -4,36 +4,40 @@ class BeersController < ApplicationController
     def index
         @beers = policy_scope(Beer)
     end
-    
+
     def new
         @breweries = Brewery.all
         @styles = Style.all
         @beer = Beer.new
         authorize @beer
     end
-    
+
     def create
-        @beer = Beer.new()
+        @beer = Beer.new(beer_params)
         authorize @beer
-        @beerbrewery = Brewery.find(params.require(:beer).permit(:brewery).attributes.values)
-        @beer.brewery = @beerbrewery
-        @beerstyle = Style.find(params.require(:beer).permit(:style))
-        @beer.style= @beerstyle
         @beer.save
-        redirect_to beer_path(@beer)
+        if @beer.save
+            redirect_to beer_path(@beer)
+        else
+          render new
+        end
     end
-    
+
     def show
+        @brewery = Brewery.find(@beer.brewery_id)
+        @style = Style.find(@beer.style_id)
     end
-    
+
     def edit
+      @breweries = Brewery.all
+      @styles = Style.all
     end
-    
+
     def update
         @beer.update(beer_params)
         redirect_to beer_path(@beer)
     end
-    
+
     def destroy
         @beer.destroy
         redirect_to beers_path
@@ -45,8 +49,8 @@ class BeersController < ApplicationController
       @beer = Beer.find(params[:id])
       authorize @beer
     end
-  
+
     def beer_params
-      params.require(:beer).permit(:name, :rating, :price, :desc)
+      params.require(:beer).permit(:name, :rating, :price, :desc, :brewery_id, :style_id)
     end
 end
